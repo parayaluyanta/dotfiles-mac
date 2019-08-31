d6d87e53075f08de0a6b231693e10ce8e50092b3
Upadhyay <prayag.upd@gmail.com>
2019-08-31 16:37:36 -0700

    untested projectile

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -91,3 +91,8 @@
 (unless (package-installed-p 'projectile)
   (package-install 'projectile))
 
+(require 'projectile)
+(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+(projectile-mode +1)
+
